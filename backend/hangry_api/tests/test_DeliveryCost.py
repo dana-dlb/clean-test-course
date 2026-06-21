c8c51381c1f0485befa12f8dab3277e30f4f5b0f
dana-dlb <69677422+dana-dlb@users.noreply.github.com>
2026-06-21 14:08:02 +0200

    Update default delivery fee

--- backend/hangry_api/tests/test_DeliveryCost.py
+++ backend/hangry_api/tests/test_DeliveryCost.py
@@ -1,37 +1,44 @@
+from types import SimpleNamespace
+
 from api.controllers import Delivery
-from django_mock_queries.query import MockSet, MockModel
+
 
 def test_LotsOfItems():
-  #Arrange
-  order = MockSet()
-  order.add(MockModel(quantity=5))
-  order.add(MockModel(quantity=5))
-  order.add(MockModel(quantity=5))
+  # Arrange
+  order = [
+    SimpleNamespace(quantity=5),
+    SimpleNamespace(quantity=5),
+    SimpleNamespace(quantity=5),
+  ]
   delivery_distance = 6
-  #Act
-  cost = Delivery.calculate(order,delivery_distance)
-  #Assert
+  # Act
+  cost = Delivery.calculate(order, delivery_distance)
+  # Assert
   assert cost == 7.5
 
+
 def test_MiddleOfTheRoadItems():
-  #Arrange
-  order = MockSet()
-  order.add(MockModel(quantity=2))
-  order.add(MockModel(quantity=2))
-  order.add(MockModel(quantity=2))
+  # Arrange
+  order = [
+    SimpleNamespace(quantity=2),
+    SimpleNamespace(quantity=2),
+    SimpleNamespace(quantity=2),
+  ]
   delivery_distance = 4
-  #Act
-  cost = Delivery.calculate(order,delivery_distance)
-  #Assert
+  # Act
+  cost = Delivery.calculate(order, delivery_distance)
+  # Assert
   assert cost == 5
 
+
 def test_LittleItems():
-  #Arrange
-  order = MockSet()
-  order.add(MockModel(quantity=3))
-  order.add(MockModel(quantity=1))
+  # Arrange
+  order = [
+    SimpleNamespace(quantity=3),
+    SimpleNamespace(quantity=1),
+  ]
   delivery_distance = 2
-  #Act
-  cost = Delivery.calculate(order,delivery_distance)
-  #Assert
-  assert cost == 2.5
+  # Act
+  cost = Delivery.calculate(order, delivery_distance)
+  # Assert
+  assert cost == 3.5
